c5585a49c8f84ca92fbd9ca308068f840c5c384d
Sai Sharan <saisharan49@gmail.com>
2026-04-03 15:56:17 -0500

    changes

--- workflows/bq_dag.py
+++ workflows/bq_dag.py
@@ -5,7 +5,7 @@ from airflow.utils.dates import days_ago
 from airflow.providers.google.cloud.operators.bigquery import BigQueryInsertJobOperator
 
 # Define constants
-PROJECT_ID = "avd-databricks-demo"
+PROJECT_ID = "retailer-datalake-project-05022026"
 LOCATION = "US"
 SQL_FILE_PATH_1 = "/home/airflow/gcs/data/BQ/bronzeTable.sql"
 SQL_FILE_PATH_2 = "/home/airflow/gcs/data/BQ/silverTable.sql"
--- workflows/pyspark_dag.py
+++ workflows/pyspark_dag.py
@@ -10,10 +10,10 @@ from airflow.providers.google.cloud.operators.dataproc import (
 )
 
 # define the variables
-PROJECT_ID = "avd-databricks-demo"
+PROJECT_ID = "retailer-datalake-project-05022026"
 REGION = "us-east1"
 CLUSTER_NAME = "my-demo-cluster"
-COMPOSER_BUCKET = "us-central1-demo-instance-9e2e6451-bucket"
+COMPOSER_BUCKET = "us-central1-demo-instance-fde062e8-bucket"
 
 GCS_JOB_FILE_1 = f"gs://{COMPOSER_BUCKET}/data/INGESTION/retailerMysqlToLanding.py"
 PYSPARK_JOB_1 = {
@@ -38,7 +38,7 @@ PYSPARK_JOB_3 = {
 
 
 ARGS = {
-    "owner": "SHAIK SAIDHUL",
+    "owner": "Sai Sharan",
     "start_date": None,
     "depends_on_past": False,
     "email_on_failure": False,
